871fdf4a5d040ce9eae902c2c092ddba4ff3b7ad
N4ND4 <47235064+Nanda-N4@users.noreply.github.com>
2026-04-19 23:48:26 +0630

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,14 @@ TOKEN = os.getenv('BOT_TOKEN')
 ADMIN_ID = int(os.getenv('ADMIN_ID', 0))
 ADMIN_LINK = 'https://t.me/n4nd404'
 
+# Server များ စာရင်း
 SERVERS = {
     'S1': {
         'url': os.getenv('S1_URL'),
         'user': os.getenv('S1_USER'),
         'pass': os.getenv('S1_PASS'),
         'domain': os.getenv('S1_DOMAIN'),
-        'sub_port': os.getenv('S1_SUB_PORT'),
+        'sub_port': os.getenv('S1_SUB_PORT', '2096'),
         'vless_id': 1, 'ss_id': 2
     },
     'S2': {
@@ -21,7 +22,7 @@ SERVERS = {
         'user': os.getenv('S2_USER'),
         'pass': os.getenv('S2_PASS'),
         'domain': os.getenv('S2_DOMAIN'),
-        'sub_port': os.getenv('S2_SUB_PORT'),
+        'sub_port': os.getenv('S2_SUB_PORT', '2053'),
         'vless_id': 1, 'ss_id': 2
     }
 }
